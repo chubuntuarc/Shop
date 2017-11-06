fce09b2a0c132199060409bc5c8b4b3fec9668b0
Jesus Arciniega <dev.jesusarc@gmail.com>
2017-11-05 20:24:16 -0700

    Super nav

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Shop | Tienda de muestra</title>
+    <!-- JQueryy -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <?php include("super-nav.php"); ?>
+    <?php include("side-nav.php"); ?>
+    <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+    <h1>Hola mundo</h1>
+
+    <script type="text/javascript">
+    $(".button-collapse").sideNav();
+    </script>
+
+</body>
+</html>
---
+++ super-nav.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/super-nav.css">
+<nav>
+    <div class="nav-wrapper">
+      <!-- <a href="#" class="brand-logo">Logo</a> -->
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Aviso de privacidad</a></li>
+        <li><a href="badges.html">Ayuda</a></li>
+        <li><a href="collapsible.html"> Ingles</a></li>
+      </ul>
+    </div>
+  </nav>
