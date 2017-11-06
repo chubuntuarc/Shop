0e8c064b387cf69d324c26f1453619c8287c8ca5
Jesus Arciniega <dev.jesusarc@gmail.com>
2017-11-05 22:52:53 -0700

    Menu slide

--- side-nav.php
+++ side-nav.php
@@ -1,15 +1,19 @@
 <ul id="slide-out" class="side-nav">
     <li><div class="user-view">
-        <div class="background">
-            <img src="images/office.jpg">
-        </div>
-        <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-        <a href="#!name"><span class="white-text name">John Doe</span></a>
-        <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+        <!-- <div class="background">
+            <img src="https://www.google.com.mx/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwiix-SWoKnXAhViHGMKHYiPAU0QjBwIBA&url=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fbrown-background-picture-id636897150%3Fk%3D6%26m%3D636897150%26s%3D612x612%26w%3D0%26h%3DanKqTU4gc-CxUfYtp5XWQAj0VPFTZ2WjSf1K8eNu-ZU%3D&psig=AOvVaw0nXb5p01ywtZg96ZV0owGR&ust=1510033732744934">
+        </div> -->
+        <a href="#!user"><img height="80px" src="https://www.mautic.org/wp-content/uploads/2015/11/grav-720x340.png" alt=""></a>
+        <!-- <a href="#!name"><span class="white-text name">John Doe</span></a>
+        <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a> -->
     </div></li>
-    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
     <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
-    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><a href="#!">Categoría uno</a></li>
+    <li><a href="#!">Categoría dos</a></li>
+    <li><a href="#!">Categoría tres</a></li>
+    <li><a href="#!">Categoría cuatro</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Cuenta</a></li>
+    <li><a class="waves-effect" href="#!">Mi cuentas</a></li>
+    <li><a class="waves-effect" href="#!">Soporte</a></li>
 </ul>
