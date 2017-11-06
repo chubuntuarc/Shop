4f09d06b45fc3c797d80bc5e2b6a62faebfa7802
Jesus Arciniega <dev.jesusarc@gmail.com>
2017-11-05 22:48:03 -0700

    Atencion y redes

--- index-extra.php
+++ index-extra.php
@@ -22,5 +22,19 @@
                 </div>
             </div>
         </div>
+        <div class="col s12 m8">
+            <div class="container">
+                <h5 style="color: #8d6e63;">Atención a clientes</h5>
+                <p>Lunes a Viernes 08:00 a 18:00 Hrs.</p>
+                <p>Tel: (555) 123 432 256</p>
+                <hr style="color:#8d6e63;">
+                <h5 style="color: #8d6e63;font-size:17px;">Nuestras redes</h5>
+                <div class="col s12 m4">
+                    <img style="border-radius:5px;" height="30px" src="https://facebookbrand.com/wp-content/themes/fb-branding/prj-fb-branding/assets/images/fb-art.png" alt="">
+                    <img style="border-radius:5px;margin-left:5px;" height="30px" src="https://n6-img-fp.akamaized.net/iconos-gratis/gorjeo_318-136405.jpg?size=338&ext=jpg" alt="">
+                    <img style="border-radius:5px;margin-left:5px;" height="30px" src="https://instagram-brand.com/wp-content/themes/ig-branding/assets/images/ig-logo-email.png" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
